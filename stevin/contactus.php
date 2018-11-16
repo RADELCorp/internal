a9b214952eebddf94857f704bbca10fa05d3e641
stevinkalex <43891769+stevinkalex@users.noreply.github.com>
2018-11-16 12:33:45 +0530

    contact us

---
+++ stevin/contactus.php
@@ -0,0 +1,140 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        
+        <link href="https://fonts.googleapis.com/css?family=Saira" rel="stylesheet">
+
+        <title>PropKorner - Contact</title>
+    </head>
+    <body>
+        <div style="height: 90px; background-color: rgb(54,34,33);">
+          
+                <table border="1" style=" color: red;margin-left: 1000px;">
+                    <thead>
+                        <tr>
+                            <th>RADEL CORP</th>
+                            <th>LOGIN/REGISTER</th>
+                            <th>Logo</th>
+                        </tr>
+                    </thead>
+                </table>
+        </div>
+        <div style="align-content: center"><h1 style="text-align: center;font-family: 'Saira', sans-serif;">CONTACT US</h1></div>
+        <div style="margin-top: 20px;" id="parent" class="col-md-12">
+            <div id="head-img" style="background-image:url(images/imgContact.jpg);background-repeat: no-repeat ;background-size: cover;  height: 300px;margin-bottom: 10px;background-position: center"></div>
+            <div id="contact-me" style="height: 300px; margin-bottom: 10px">
+                <div class="col-md-4" style="background-color:rgb(219, 219, 219); height: 300px; text-align: center;">
+                    <br/><br/><b>Reach Us</b
+                    <br/>
+                    <p style="padding-top: 15px;text-align: left; margin-left: 50px; font-family: 'Saira', sans-serif;">
+                        608, 10th Main Rd, 4th T Block East,<br/> 
+                        4th Block, Jayanagar, Bengaluru,<br/> 
+                        Karnataka 560011<br/>
+                        Email: popkorner@radelcorp.in<br/>
+                        phone:
+                        7733220044
+                    </p>
+                </div>
+                <div  class="col-md-8" style="height: 300px">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.676531213553!2d77.58313651477012!3d12.928499590884138!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae159787bab1eb%3A0xbd9499bec26c769!2sRADEL+CORP!5e0!3m2!1sen!2sin!4v1542281628249" frameborder="0" style="border:0;height: 300px;width: 980px" allowfullscreen>
+                    </iframe>
+                </div>
+            </div>
+            <div id="customer" style=" height: 300px;">
+                <div class="col-md-6" style="height: 300px;background-color: rgb(219, 219, 219);font-family: 'Saira', sans-serif;">
+                    <!--                    <div class="container" style="margin-top: 20px;">-->
+                    <!--                        <form class="form-horizontal">-->
+                    <!--                            <div class="form-group">-->
+                    <div style="margin-top: 20px;">
+                        <label class="control-label col-sm-2" for="email">Name:</label>
+                        <div class="col-sm-10">
+                            <input type="text" class=" form-control" id="usrname" placeholder="Enter Name" name="usrname" style="width: 500px">
+                        </div>
+
+                        <label class="control-label col-sm-2" for="email" style="margin-top: 20px">Email:</label>
+                        <div class="col-sm-10" style="margin-top: 20px">
+                            <input type="email" class=" form-control" id="usremail" placeholder="Enter Email" name="usremail" style="width: 500px">
+                        </div>
+                        <label class="control-label col-sm-2" for="email" style="margin-top: 20px" for="comment">Message:</label>
+                        <div class="col-sm-10" style="margin-top: 20px">
+                            <textarea class="form-control" rows="5" id="comment" style="width: 500px;"></textarea>
+                        </div>
+                        <!--                            </div>-->
+                        <div class="col-sm-offset-2 col-sm-10" style="text-align: right">
+                            <button type="submit" class="btn btn-default" >Sent</button>
+                        </div>
+                        <!--                        </form>-->
+                        <!--                    </div>-->
+                    </div>
+                </div>
+                <div class="col-md-6" style="height: 200px; width: 49%;">
+                    <b> Some information</b>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-12" style="background-color:rgb(219, 219, 219);height: 300px; margin-top: 30px ">
+            <div class="col-md-3" style="height:200px; margin-top: 20px;border-bottom: 2px solid honeydew ;font-family: 'Saira', sans-serif;">
+                <table style="border-bottom: 1px solid #dedede; font-family: inherit;">
+                    <tr>
+                        <th>
+                            <b style="border-bottom: 2px solid honeydew ;">EXPOLRE</b></th>
+                    </tr>
+                    <tr>
+                        <td>HOME</td></tr>
+                    <tr>
+                        <td>ABOUT US</td></tr>
+                    <tr>
+                        <td>CAREERS</td></tr>
+                    <tr>
+                        <td>BLOGS</td></tr>
+                </table>
+            </div>
+            <div class="col-md-3" style="height: 200px;margin-top: 20px;border-bottom: 2px solid honeydew ;font-family: 'Saira', sans-serif;"><b style="border-bottom: 2px solid honeydew ;">VISIT</b>
+                <p style="text-align: left;">
+                    608, 10th Main Rd, 4th T Block East,<br/> 
+                    4th Block, Jayanagar, Bengaluru,<br/> 
+                    Karnataka 560011<br/>
+
+                    Email: popkorner@radelcorp.in<br/>
+                    phone:
+                    7733220044
+                </p>
+            </div>
+            <div class="col-md-3" style="height: 200px;margin-top: 20px;border-bottom: 2px solid honeydew ;font-family: 'Saira', sans-serif;">      
+                <ul style="list-style-type:none">
+                    <li style="border-bottom: 2px solid honeydew ; border-image-width: 2px"><b>CONTACT US</b></li>
+                    <li>+91-8976785430</li>
+                    <li>+91-7685649870</li>
+                    <li>vvftrb@gmail.com</li>
+                </ul>
+            </div>
+            <div class="col-md-3" style="height: 200px;margin-top: 20px;border-bottom: 2px solid honeydew ; font-family: 'Saira', sans-serif;">
+                <table>
+                    <tr>
+                        <th style=" border-bottom: 2px solid honeydew;">Follow</th>
+                    </tr>
+                    <tr>
+                        <td>INSTAGRAM</td></tr>
+                    <tr>
+                        <td>TWITTER</td></tr>
+                    <tr>
+                        <td>FACEBOOK</td></tr>
+                    <tr>
+                        <td>GOOGLE+</td></tr>
+                </table>
+            </div>
+            <div style="font-family: 'Saira', sans-serif;">
+            <b style="margin-bottom: 90px; ">@2018. All Rights Reserved</b>
+            <b style="margin-left: 1000px;">RADEL CORP</b>
+            </div>
+        </div>
+    </body>
+</html>
+
+
