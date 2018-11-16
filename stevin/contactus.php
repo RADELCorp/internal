ab93e5232a1d00bb83b6dc47cb7fadb6289cfb05
stevinkalex <43891769+stevinkalex@users.noreply.github.com>
2018-11-16 14:29:46 +0530

    Update contactus.php
    contact US

--- stevin/contactus.php
+++ stevin/contactus.php
@@ -42,8 +42,8 @@
                         7733220044
                     </p>
                 </div>
-                <div  class="col-md-8" style="height: 300px">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.676531213553!2d77.58313651477012!3d12.928499590884138!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae159787bab1eb%3A0xbd9499bec26c769!2sRADEL+CORP!5e0!3m2!1sen!2sin!4v1542281628249" frameborder="0" style="border:0;height: 300px;width: 980px" allowfullscreen>
+                <div  class="col-md-8" style="height: 300px; padding-right: 0;">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.676531213553!2d77.58313651477012!3d12.928499590884138!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae159787bab1eb%3A0xbd9499bec26c769!2sRADEL+CORP!5e0!3m2!1sen!2sin!4v1542281628249" frameborder="0" style="border:0;height: 300px;width:100%" allowfullscreen>
                     </iframe>
                 </div>
             </div>
