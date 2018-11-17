85f3fd65d050dfe0e750bd810302eaaeed6dadb1
jintuabraham thomas <44797802+jintuabraham@users.noreply.github.com>
2018-11-17 12:45:13 +0530

    Update test.php
    updated contact US

--- jintu/test.php
+++ jintu/test.php
@@ -2,7 +2,7 @@
     <head>
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
+<link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed" rel="stylesheet">
         <!-- jQuery library -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
@@ -10,17 +10,24 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
     <body>
-        <div id="parent">
-            <div style="background-color: blueviolet; height: 300px;background-image: url( justin-schuler-253611-unsplash.jpg);background-size: cover; background-position: center"> </div>
-            <div  class="row" style="background-color: white; height: 250px">
-                <div class="col-md-6" style="background-color:#dbdbdb;height: 250px"><b>REACH US</b><p>608,10th main Rd,4thBlock,Jaynagar,Bengaluru<br><p>Karnataka 560011<br><p>Email:Porpkorner@radelcorp.in<br><p>Mobile Number:8877445566<br><p>7733220044</p></div>
-                <div id="googleMap" class="col-md-6">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62219.05992157522!2d77.5642236012548!3d12.927555141612515!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae15a20cea94d1%3A0x77fa77df36f6afa7!2sRADEL+CORP!5e0!3m2!1sen!2sin!4v1542281785760" width="650" height="250" frameborder="0" style="border:0" allowfullscreen>
+
+
+
+        <div class="col-md-12" id="parent" style="font-family: 'Saira Extra Condensed', sans-serif;">
+            <div class="col-md-12"style="height: 300px;background-image: url( justin-schuler-253611-unsplash.jpg);background-size: cover; background-position: center"> 
+            </div>
+            
+            <div  class="col-md-12" style="background-color: white; height: 300px;padding: 0;margin-top: 10px;">
+                <div class="col-md-4" style="background-color:#dbdbdb;height: 300px;padding-left: 0;">
+                    <b style="margin-left: 100px;margin-top: 10px">REACH US</b><p style="margin-left: 50px; margin-top: 20px">608,10th main Rd,4thBlock,Jaynagar,Bengaluru<br/>Karnataka 560011<br/>Email:Porpkorner@radelcorp.in<br/>Mobile Number:8877445566<br/>7733220044</p>
+                </div>
+                <div id="googleMap" class="col-md-8" style="padding-right: 0;">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62219.05992157522!2d77.5642236012548!3d12.927555141612515!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae15a20cea94d1%3A0x77fa77df36f6afa7!2sRADEL+CORP!5e0!3m2!1sen!2sin!4v1542281785760" frameborder="0" style="border:0;width: 100%;height: 300px;" allowfullscreen>
 
                     </iframe>
                 </div>
             </div>
-            <div id="customer" class="col-md-12" style="background-color:lightslategray ; height: 300px;">
+            <div id="customer" class="col-md-12" style="background-color:lightslategray ; height: 300px;margin-top: 10px;">
                 <div class="col-md-6" style="height: 200px; width: 49%;">
                     <div class="container" style="margin-top: 20px;">
                         <form class="form-horizontal">
@@ -50,41 +57,50 @@
                 </form>
 
             </div>
-            <div class="col-md-12" style="background-color:#dbdbdb;height: 300px;">
-                <div class="col-md-3"style="height: 300px;">
+            <div class="col-md-12" style="background-color:#dbdbdb;height:300px;margin-top: 10px">
+
+                <div class="col-md-3"style="height: 200px;">
                     <ul style="list-style-type: none">
                         <li style="font-weight: bold">EXPLORE</li>
                         <li>ABOUT US</li>
                         <li>CAREERS</li>
                         <li>BLOGS</li>
-
                     </ul>
                 </div>
 
-                <div class="col-md-3"style="height: 300px;">
-                    <ul style="list-style-type: none">
-                        <li style="font-weight: bold">VISIT</li>
-                        <li>NO2,Ground floor 29thmain Road<b>kevempu Nagar<b>Kuvempu Nagar</b><b>BTM Layout 2nd</b><b>stage bangalore<b>Karnataka 560076</b></b></b></li>
-                    </ul>
-                </div>
-                <div class="col-md-3"style="height: 300px;">
-                    <ul style="list-style-type: none">
-                        <li style="font-weight: bold">CONTACT US</li>
-                        <li>8945687598</li>
-                        <li>8945687598</li>
-                    </ul>
-                </div>
-                <div class="col-md-3"style="height: 300px;">
-                    <ul style="list-style-type: none">
-                        <li style="font-weight: bold">FOLLOW</li>
-                        <li>INSTAGRAM</li>
-                        <li>TWITTER</li>
-                        <li>FACEBOOK</li>
-                        <li>GOOGLE</li>
-                    </ul>
-                </div>
-                
+            
+
+            <div class="col-md-3"style="height: 200px;">
+                <ul style="list-style-type: none">
+                    <li style="font-weight: bold">VISIT</li>
+                    <li>NO2,Ground floor 29thmain Road<b/><b>Kuvempu Nagar</b><b>BTM Layout 2nd</b><b>stage bangalore<b>Karnataka 560076</b></b></b></li>
+                </ul>
             </div>
-        </div>
+            <div class="col-md-3"style="height: 200px;">
+                <ul style="list-style-type: none">
+                    <li style="font-weight: bold">CONTACT US</li>
+                    <li>8945687598</li>
+                    <li>8945687598</li>
+                </ul>
+            </div>
+            <div class="col-md-3"style="height: 200px;">
+                <ul style="list-style-type: none">
+                    <li style="font-weight: bold">FOLLOW</li>
+                    <li>INSTAGRAM</li>
+                    <li>TWITTER</li>
+                    <li>FACEBOOK</li>
+                    <li>GOOGLE</li>
+                </ul>
+            </div>
+                 <div>
+                <ul style="list-style-type: none">
+                    
+                    <b> &COPY;2018,ALL Reserved </b>
+                    <b style="margin-left: 1000px">RADEL CORP</b>
+            </div>
+            </div>
+           
+            
+            </div>
     </body>
 </html>
