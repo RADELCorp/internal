fadd04af565443da4946f44faa3548be59fa98f9
Akhil P Nair <pakhilnair@gmail.com>
2018-11-17 14:39:02 +0530

    Property Details Page
    Build 1.

---
+++ akhil/details.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Property Details</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="owlcarousel/owl.carousel.min.css">
+        <link rel="stylesheet" href="owlcarousel/owl.theme.default.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="jquery.min.js"></script>
+        <script src="owlcarousel/owl.carousel.min.js"></script>
+        <link href="OwlCarousel2-2.3.4/dist/assets/owl.carousel.css" rel="stylesheet" type="text/css">
+        <link href="OwlCarousel2-2.3.4/dist/assets/owl.theme.default.min.css" rel="stylesheet" type="text/css">
+        <meta charset="utf-8">
+        <style type="text/css">
+            *{
+                margin: 0;
+                padding: 0;
+            }
+            .img-fluid{
+                max-width: 100%;
+                height: 100%;
+                display: block; 
+            }
+        </style>
+    </head>
+    <body>
+        <div class="nav" style="background-color: black; opacity: 0.9;"> 
+            <center><h3>PROP KORNER</h3></center>
+        </div>
+        <div class="main" style="margin-left:10%; margin-right: 10%">
+        <div class="container">
+           <!-- <div class="row">   
+                <div class="col-md-2"> 
+                    <input type="button" value="Back">
+                </div>   -->
+            <div class="col-md=6" style="height: 50px">
+                <h3 style=" font-weight: bold "> Viemann Complex </h3>
+            </div>
+        </div>
+            <div class="col-md-12" style="height: 500px;background-image:url(images/aaa.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;"> </div>
+      <!--      <div class="abc" style="align-content:">
+                <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left"></span>
+                    <span class="sr-only">Previous</span>
+                </a>  -->
+      <div class="owl-carousel">
+            <div class="col-md-2" style="height: 150px;background-image:url(images/apartments.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;"></div>
+            <div class="col-md-2" style="height: 150px;background-image:url(images/aaa.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;"></div>
+            <div class="col-md-2" style="height: 150px;background-image:url(images/bbb.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;"></div>
+            <div class="col-md-2" style="height: 150px;background-image:url(images/villa.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;"></div>
+            <div class="col-md-2" style="height: 150px;background-image:url(images/more.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;"></div>
+            <div class="col-md-2" style="height: 150px;background-image:url(images/aaa.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;"></div>
+      </div>
+      <div class="owl-carousel owl-theme">
+        <div> <img src="images/aaa.jpg"></div>
+        <div> <img src="images/bbb.jpg"></div>
+        <div> <img src="images/aaa.jpg"></div>
+        <div> <img src="images/bbb.jpg"></div>
+      </div>
+        <script src="OwlCarousel2-2.3.4/docs/assets/vendors/jquery.min.js" type="text/javascript"> </script>
+        <script src="OwlCarousel2-2.3.4/dist/owl.carousel.js" type="text/javascript"> </script>
+        <script type="text/javascript">
+             $('.owl-carousel').owlCarousel({
+             loop:true,
+             margin:10,
+             nav:true,
+             responsive:{
+                 0:{
+                 items:1
+                },
+              600:{
+               items:3
+              },
+              1000:{
+                    items:5
+           }
+    }
+})
+    </script
+        </br>
+        <div class="col-md-12" style="text-align: left">
+            <h3 style="position: relative; font-weight: inherit; text-align: center">
+                lorem ipsum dolor sit amet. lorem ipsum dolor sit amet. lorem ipsum dolor sit amethyst.
+                lorem ipsum dolor sit amet. lorem ipsum dolor sit amet. lorem ipsum dolor sit amethyst.
+                lorem ipsum dolor sit amet. lorem ipsum dolor sit amet. lorem ipsum dolor sit amethyst.
+                </h3>
+            </br> </br> </br>
+            <div class="location" style="text-align:center">
+                <h3 style="font-weight:bold"> Our Location </h3>
+                </br>
+            </div>
+            <iframe src="https://www.google.com/maps/embed?pb=" width="100%" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+    </div>
+    </div>
+</body>
+</html>
