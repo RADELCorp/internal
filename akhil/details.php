27978f402b1afa9b3c2f44f7dbf6b328544ea22c
Akhil P Nair <pakhilnair@gmail.com>
2018-11-20 12:16:15 +0530

    Update details.php

--- akhil/details.php
+++ akhil/details.php
@@ -7,29 +7,31 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
         <link rel="stylesheet" href="ohttps://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">  
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<!--        <script src="jquery.min.js"></script>-->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
         <link href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.css" rel="stylesheet" type="text/css">
         <link href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" rel="stylesheet" type="text/css">
         <meta charset="utf-8">
         <style type="text/css">
-            *{
-                margin: 0;
-                padding: 0;
-            }
             .img-fluid{
                 max-width: 100%;
                 height: 100%;
                 display: block; 
             }
             body {
-                font-family: 'Saira Extra Condensed', sans-serif;
+                font-family: 'Raleway', sans-serif;
                 background-color: #ECEFF;
             }
             .footer {
                 color: white
+            }
+            .shadow {
+                box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+            }
+            a {
+                color: white;
             }
         </style>
     </head>
@@ -49,15 +51,15 @@
                 <h3 style=" font-weight: bold; font-size: 220% "> Viemann Complex </h3>
             </div>
           </div>
-          <div class="col-md-12" style="height: 500px;background-image:url(images/aaa.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;"> </div>
-          <div class="owl-carousel">
+      <div class="col-md-12" style="height: 500px;background-image:url(images/aaa.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;"> </div>
+        <div class="owl-carousel">
             <div class="col-md-2" style="height: 150px;background-image:url(images/apartments.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;"></div>
             <div class="col-md-2" style="height: 150px;background-image:url(images/aaa.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;"></div>
             <div class="col-md-2" style="height: 150px;background-image:url(images/bbb.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;"></div>
             <div class="col-md-2" style="height: 150px;background-image:url(images/villa.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;"></div>
             <div class="col-md-2" style="height: 150px;background-image:url(images/more.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;"></div>
             <div class="col-md-2" style="height: 150px;background-image:url(images/aaa.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;"></div>
-           </div>
+        </div>
       <div class="owl-carousel owl-theme">
         <div> <img src="images/aaa.jpg"></div>
         <div> <img src="images/bbb.jpg"></div>
@@ -84,64 +86,83 @@
     }
 })
     </script
-        </br>
+        </br> </br>
         <div class="col-md-12" style="text-align: left">
-            <h3 style="position: relative; font-weight: inherit; text-align: center">
-                lorem ipsum dolor sit amet. lorem ipsum dolor sit amet. lorem ipsum dolor sit amethyst.
-                lorem ipsum dolor sit amet. lorem ipsum dolor sit amet. lorem ipsum dolor sit amethyst.
-                lorem ipsum dolor sit amet. lorem ipsum dolor sit amet. lorem ipsum dolor sit amethyst.
-                </h3>
-            </br> </br> </br>
-            <div class="location" style="text-align:center">
+            <div class="col-md-6" style="text-align: left; background-color:#E0E0E0; height:180px; border-right-style: solid;border-right-color:white;">
+                <h3 style="text-align: justify">DESCRIPTION </h3>
+                <h5 style="position: relative; font-weight: inherit; text-align: left">
+                    lorem ipsum dolor sit amet. lorem ipsum dolor sit amet. lorem ipsum dolor sit amethyst.
+                    lorem ipsum dolor sit amet. lorem ipsum dolor sit amet. lorem ipsum dolor sit amethyst.
+                    lorem ipsum dolor sit amet. lorem ipsum dolor sit amet. lorem ipsum dolor sit amethyst.
+                    lorem ipsum dolor sit amet. lorem ipsum dolor sit amet. lorem ipsum dolor sit amethyst.
+                    lorem ipsum dolor sit amet. lorem ipsum dolor sit amet. lorem ipsum dolor sit amethyst.
+                </h5>
+            </div>
+            <div class="col-md-6" style="text-align: left; background-color:#E0E0E0; height:180px;">
+                <h3 style="text-align: left">PROPERTY INFORMATION </h3>
+                <div class="col-md-3" style="line-height:2">
+                       <p>Variable 1 : " "
+                       Variable 2 : " "
+                       Variable 3 : " "
+                       Variable 4 : " "</p>  
+                </div>
+                <div class="col-md-3" style="text-align = center; line-height:2">
+                       <p>Variable 5 : " "
+                       Variable 6 : " "
+                       Variable 7 : " "
+                       Variable 8 : " "</p>    
+                </div>
+        </div>
+            <div class="col-md-12 location" style="text-align:center">
+                </br>
                 <h3 style="font-weight:bold"> Our Location </h3>
                 </br>
             </div>
-            <div class="col-md-12" style="text-align: left">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15554.77134221551!2d77.5905968!3d12.92745305!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x77fa77df36f6afa7!2sRADEL+CORP!5e0!3m2!1sen!2sin!4v1542622320218" width="100%" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <div class="col-md-12">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15554.77134221551!2d77.5905968!3d12.92745305!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x77fa77df36f6afa7!2sRADEL+CORP!5e0!3m2!1sen!2sin!4v1542622320218" width="100%" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
             </div>
-           </div>
+            </div>
     </div>
     </br> </br>
     <script type="text/javascript">
-        $(document).ready(function(){
-        $('.owl-carousel').owlCarousel({
-        nav:true,
-        responsive:{
-        ...
-        })
-        $( ".owl-prev").html('<i class="fa fa-chevron-left"></i>');
-        $( ".owl-next").html('<i class="fa fa-chevron-right"></i>');
-        });
-    </script>
+        $(".category-wrapper").owlCarousel({
+        items : 4,
+        loop  : true,
+        margin : 30,
+        nav    : true,
+        smartSpeed :900,
+        navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"]
+            });
+    </script>   
     <div class="footer">
             <div class="row shadow">
                 <div class="col-md-12 col-sm-12" style="background-color:black;height: 350px;">
                     <div class="col-md-3 col-sm-3 col-xs-3" style="height: 70%;padding-left: 8%">
                         <p style="font-size:23px;padding-top: 10%;"><b>EXPLORE</b></p>
                         <hr style="margin-top:1%" width="15%;" align="left">
-                        <p style="font-size:16px;padding-top: 1%">HOME</p>
-                        <p style="font-size:16px;padding-top: 1%">ABOUT US</p>
-                        <p style="font-size:16px;padding-top: 1%">CAREER</p>
-                        <p style="font-size:16px;padding-top: 1%">BLOGS</p>
+                        <p style="font-size:14px;padding-top: 1%">HOME</p>
+                        <p style="font-size:14px;padding-top: 1%">ABOUT US</p>
+                        <p style="font-size:14px;padding-top: 1%">CAREER</p>
+                        <p style="font-size:14px;padding-top: 1%">BLOGS</p>
                     </div>
                     <div class="col-md-3 col-sm-3 col-xs-3" style="height: 70%;padding-left: 8%">
                         <p style="font-size:23px;padding-top: 10%"><b>VISIT</b></p>
                         <hr style="margin-top:1%" width="15%;" align="left">
-                        <p style="font-size:16px;padding-top: 1%"> No 2, Ground floor,<br> 29th Main Road,<br> Kuvempu Nagar,<br> BTM Layout 2nd Stage,<br> Bengaluru,<br> Karnataka 560076</p>
+                        <p style="font-size:14px;padding-top: 1%"> No 2, Ground floor,<br> 29th Main Road,<br> Kuvempu Nagar,<br> BTM Layout 2nd Stage,<br> Bengaluru,<br> Karnataka 560076</p>
                     </div>
                     <div class="col-md-3 col-sm-3 col-xs-3" style="height: 70%;padding-left: 8%">
                         <p style="font-size:23px;padding-top: 10%"><b>CONTACT US</b></p>
                         <hr style="margin-top:1%" width="15%;" align="left">
-                        <p style="font-size:16px;padding-top: 1%">+91-8945687598</p>
-                        <p style="font-size:16px;padding-top: 1%">+91-8945687598</p>
+                        <p style="font-size:14px;padding-top: 1%">+91-8945687598</p>
+                        <p style="font-size:14px;padding-top: 1%">+91-8945687598</p>
                     </div>
                     <div class="col-md-3 col-sm-3 col-xs-3" style="height: 70%;padding-left: 8%">
                         <p style="font-size:23px;padding-top: 10%"><b>FOLLOW</b></p>
                         <hr style="margin-top:1%" width="15%;" align="left">
-                        <p style="font-size:16px;padding-top: 1%"><a href="#">INSTAGRAM </a></p>
-                        <p style="font-size:16px;padding-top: 1%"><a href="#">TWITTER </a></p>
-                        <p style="font-size:16px;padding-top: 1%"><a href="#">GOOGLE+ </a></p>
-                        <p style="font-size:16px;padding-top: 1%"><a href="#">FACEBOOK </a></p>
+                        <p style="font-size:14px;padding-top: 1%"><a href="#">INSTAGRAM </a></p>
+                        <p style="font-size:14px;padding-top: 1%"><a href="#">TWITTER </a></p>
+                        <p style="font-size:14px;padding-top: 1%"><a href="#">GOOGLE+ </a></p>
+                        <p style="font-size:14px;padding-top: 1%"><a href="#">FACEBOOK </a></p>
                     </div>
                     <hr style="margin-top:1%" width="95%;">
                     <div class="col-md-6 col-sm-6 col-xs-6" style="height: 30%;padding-left: 8%;">
