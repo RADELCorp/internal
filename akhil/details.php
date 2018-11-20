76f9ab84cc1c6f78bc11ff3cc016c7b1d573ca60
Akhil P Nair <pakhilnair@gmail.com>
2018-11-20 13:39:31 +0530

    Property Details Page.

--- akhil/details.php
+++ akhil/details.php
@@ -27,7 +27,7 @@
             .footer {
                 color: white
             }
-            .shadow {
+            .footer {
                 box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
             }
             a {
@@ -35,58 +35,60 @@
             }
         </style>
     </head>
+
     <body>
         <div class="nav" style="background-color: black; opacity: 0.9;"> 
             <center><h3>PROP KORNER</h3></center>
         </div>
-<!--        <div class="main" style="margin-left:10%; margin-right: 10%">-->
+        <!--        <div class="main" style="margin-left:10%; margin-right: 10%">-->
         <div class="container">
-          <div class="row">
-            <div class="col-md=12" style="height: 50px">
-              <div class="col-md-1">
-                <button class="btn">
-            <span class="glyphicon glyphicon-chevron-left"></span> </button>
-            </div>
-            <div class="text" style="height: 50px">
-                <h3 style=" font-weight: bold; font-size: 220% "> Viemann Complex </h3>
-            </div>
-          </div>
-      <div class="col-md-12" style="height: 500px;background-image:url(images/aaa.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;"> </div>
-        <div class="owl-carousel">
-            <div class="col-md-2" style="height: 150px;background-image:url(images/apartments.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;"></div>
-            <div class="col-md-2" style="height: 150px;background-image:url(images/aaa.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;"></div>
-            <div class="col-md-2" style="height: 150px;background-image:url(images/bbb.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;"></div>
-            <div class="col-md-2" style="height: 150px;background-image:url(images/villa.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;"></div>
-            <div class="col-md-2" style="height: 150px;background-image:url(images/more.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;"></div>
-            <div class="col-md-2" style="height: 150px;background-image:url(images/aaa.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;"></div>
-        </div>
-      <div class="owl-carousel owl-theme">
-        <div> <img src="images/aaa.jpg"></div>
-        <div> <img src="images/bbb.jpg"></div>
-        <div> <img src="images/aaa.jpg"></div>
-        <div> <img src="images/bbb.jpg"></div>
-      </div>
-        <script src="OwlCarousel2-2.3.4/docs/assets/vendors/jquery.min.js" type="text/javascript"> </script>
-        <script src="OwlCarousel2-2.3.4/dist/owl.carousel.js" type="text/javascript"> </script>
-        <script type="text/javascript">
-             $('.owl-carousel').owlCarousel({
-             loop:true,
-             margin:10,
-             nav:true,
-             responsive:{
-                 0:{
-                 items:1
+            <div class="row">
+                <div class="col-md=12" style="height: 50px">
+                    <div class="col-md-1">
+                        <button class="btn">
+                            <span class="glyphicon glyphicon-chevron-left"></span> </button>
+                    </div>
+                    <div class="text" style="height: 50px">
+                        <h3 style=" font-weight: bold; font-size: 220% "> Viemann Complex </h3>
+                    </div>
+                </div>
+                <div class="col-md-12" style="height: 500px;background-image:url(images/aaa.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;"> </div>
+            
+                <div class="col-md-12">
+                     <div class="owl-carousel owl-theme">
+                         <div> <img src="images/aaa.jpg"></div>
+                         <div> <img src="images/bbb.jpg"></div>
+                         <div> <img src="images/aaa.jpg"></div>
+                         <div> <img src="images/bbb.jpg"></div>
+                         <div> <img src="images/aaa.jpg"></div>
+                         <div> <img src="images/bbb.jpg"></div>
+                     </div>
+                 </div>
+                <script src="Owl_files/jquery.min.js" type="text/javascript"> </script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.js" type="text/javascript"> </script>
+<!--                <script type="text/javascript">
+                    $('.owl-carousel').owlCarousel({
+                     loop:true,
+                     margin:10,
+                     responsiveClass:true,
+                        responsive:{
+                            0:{
+                            items:1,
+                            nav:true
+                            },
+                            600:{
+                                items:3,
+                        nav:false
                 },
-              600:{
-               items:3
-              },
-              1000:{
-                    items:5
-           }
-    }
-})
-    </script
-        </br> </br>
+                1000:{
+                    items:5,
+                    nav:true,
+                    loop:false
+                }
+            }
+        })
+                </script>-->
+    <br/> <br/>
         <div class="col-md-12" style="text-align: left">
             <div class="col-md-6" style="text-align: left; background-color:#E0E0E0; height:180px; border-right-style: solid;border-right-color:white;">
                 <h3 style="text-align: justify">DESCRIPTION </h3>
@@ -112,67 +114,106 @@
                        Variable 7 : " "
                        Variable 8 : " "</p>    
                 </div>
-        </div>
+            </div>
             <div class="col-md-12 location" style="text-align:center">
-                </br>
+                <br/>
                 <h3 style="font-weight:bold"> Our Location </h3>
-                </br>
+                <br/>
             </div>
             <div class="col-md-12">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15554.77134221551!2d77.5905968!3d12.92745305!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x77fa77df36f6afa7!2sRADEL+CORP!5e0!3m2!1sen!2sin!4v1542622320218" width="100%" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+                <br/>
             </div>
+            <script type="text/javascript">
+                $(".category-wrapper").owlCarousel({
+                items : 4,
+                loop  : true,
+                margin : 30,
+                nav    : true,
+                smartSpeed :900,
+                navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"]
+                    });
+            </script>
+            <div class="text" style="text-align:center">
+                <h3> Similar properties </h3>
+                <br/>
             </div>
-    </div>
-    </br> </br>
-    <script type="text/javascript">
-        $(".category-wrapper").owlCarousel({
-        items : 4,
-        loop  : true,
-        margin : 30,
-        nav    : true,
-        smartSpeed :900,
-        navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"]
-            });
-    </script>   
-    <div class="footer">
-            <div class="row shadow">
-                <div class="col-md-12 col-sm-12" style="background-color:black;height: 350px;">
-                    <div class="col-md-3 col-sm-3 col-xs-3" style="height: 70%;padding-left: 8%">
-                        <p style="font-size:23px;padding-top: 10%;"><b>EXPLORE</b></p>
-                        <hr style="margin-top:1%" width="15%;" align="left">
-                        <p style="font-size:14px;padding-top: 1%">HOME</p>
-                        <p style="font-size:14px;padding-top: 1%">ABOUT US</p>
-                        <p style="font-size:14px;padding-top: 1%">CAREER</p>
-                        <p style="font-size:14px;padding-top: 1%">BLOGS</p>
-                    </div>
-                    <div class="col-md-3 col-sm-3 col-xs-3" style="height: 70%;padding-left: 8%">
-                        <p style="font-size:23px;padding-top: 10%"><b>VISIT</b></p>
-                        <hr style="margin-top:1%" width="15%;" align="left">
-                        <p style="font-size:14px;padding-top: 1%"> No 2, Ground floor,<br> 29th Main Road,<br> Kuvempu Nagar,<br> BTM Layout 2nd Stage,<br> Bengaluru,<br> Karnataka 560076</p>
-                    </div>
-                    <div class="col-md-3 col-sm-3 col-xs-3" style="height: 70%;padding-left: 8%">
-                        <p style="font-size:23px;padding-top: 10%"><b>CONTACT US</b></p>
-                        <hr style="margin-top:1%" width="15%;" align="left">
-                        <p style="font-size:14px;padding-top: 1%">+91-8945687598</p>
-                        <p style="font-size:14px;padding-top: 1%">+91-8945687598</p>
-                    </div>
-                    <div class="col-md-3 col-sm-3 col-xs-3" style="height: 70%;padding-left: 8%">
-                        <p style="font-size:23px;padding-top: 10%"><b>FOLLOW</b></p>
-                        <hr style="margin-top:1%" width="15%;" align="left">
-                        <p style="font-size:14px;padding-top: 1%"><a href="#">INSTAGRAM </a></p>
-                        <p style="font-size:14px;padding-top: 1%"><a href="#">TWITTER </a></p>
-                        <p style="font-size:14px;padding-top: 1%"><a href="#">GOOGLE+ </a></p>
-                        <p style="font-size:14px;padding-top: 1%"><a href="#">FACEBOOK </a></p>
-                    </div>
-                    <hr style="margin-top:1%" width="95%;">
-                    <div class="col-md-6 col-sm-6 col-xs-6" style="height: 30%;padding-left: 8%;">
-                        <p style="font-size:21px;padding-top: 2%;"><b>2018. All Rights Reserved</b></p>
-                    </div>
-                    <div class="col-md-6 col-sm-6 col-xs-6" style="height: 30%;padding-right: 8%;">
-                        <p style="font-size:21px;padding-top: 2%;text-align: right"><b>RADEL CORP.</b></p>
-                    </div>
+            <div class="container">
+            <div class="col-md-12">
+                <div class="owl-carousel owl-theme">
+                    <div> <img src="images/aaa.jpg"></div>
+                    <div> <img src="images/bbb.jpg"></div>
+                    <div> <img src="images/aaa.jpg"></div>
+                    <div> <img src="images/bbb.jpg"></div>
+                    <div> <img src="images/aaa.jpg"></div>
+                    <div> <img src="images/bbb.jpg"></div>
                 </div>
-            </div>           <!-- Footer : Darshan's Code  -->
-    </div>  
-</body>
-</html>
+            </div>
+            </div>
+            <script src="Owl_files/jquery.min.js" type="text/javascript"> </script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.js" type="text/javascript"> </script>
+            <script type="text/javascript">
+                $('.owl-carousel').owlCarousel({
+                 loop:true,
+                 margin:10,
+                 responsiveClass:true,
+                    responsive:{
+                        0:{
+                        items:1,
+                        nav:true
+                        },
+                        600:{
+                            items:3,
+                    nav:false
+            },
+            1000:{
+                items:5,
+                nav:true,
+                loop:false
+            }
+        }
+    })
+            </script>
+            <div class="footer">
+                <div class="row shadow">
+                    <div class="col-md-12 col-sm-12" style="background-color:black;height: 350px;">
+                        <div class="col-md-3 col-sm-3 col-xs-3" style="height: 70%;padding-left: 8%">
+                            <p style="font-size:23px;padding-top: 10%;"><b>EXPLORE</b></p>
+                            <hr style="margin-top:1%" width="15%;" align="left">
+                            <p style="font-size:14px;padding-top: 1%">HOME</p>
+                            <p style="font-size:14px;padding-top: 1%">ABOUT US</p>
+                            <p style="font-size:14px;padding-top: 1%">CAREER</p>
+                            <p style="font-size:14px;padding-top: 1%">BLOGS</p>
+                        </div>
+                        <div class="col-md-3 col-sm-3 col-xs-3" style="height: 70%;padding-left: 8%">
+                            <p style="font-size:23px;padding-top: 10%"><b>VISIT</b></p>
+                            <hr style="margin-top:1%" width="15%;" align="left">
+                            <p style="font-size:14px;padding-top: 1%"> No 2, Ground floor,<br> 29th Main Road,<br> Kuvempu Nagar,<br> BTM Layout 2nd Stage,<br> Bengaluru,<br> Karnataka 560076</p>
+                        </div>
+                        <div class="col-md-3 col-sm-3 col-xs-3" style="height: 70%;padding-left: 8%">
+                            <p style="font-size:23px;padding-top: 10%"><b>CONTACT US</b></p>
+                            <hr style="margin-top:1%" width="15%;" align="left">
+                            <p style="font-size:14px;padding-top: 1%">+91-8945687598</p>
+                            <p style="font-size:14px;padding-top: 1%">+91-8945687598</p>
+                        </div>
+                        <div class="col-md-3 col-sm-3 col-xs-3" style="height: 70%;padding-left: 8%">
+                            <p style="font-size:23px;padding-top: 10%"><b>FOLLOW</b></p>
+                            <hr style="margin-top:1%" width="15%;" align="left">
+                            <p style="font-size:14px;padding-top: 1%"><a href="#">INSTAGRAM </a></p>
+                            <p style="font-size:14px;padding-top: 1%"><a href="#">TWITTER </a></p>
+                            <p style="font-size:14px;padding-top: 1%"><a href="#">GOOGLE+ </a></p>
+                            <p style="font-size:14px;padding-top: 1%"><a href="#">FACEBOOK </a></p>
+                        </div>
+                        <hr style="margin-top:1%" width="95%;">
+                        <div class="col-md-6 col-sm-6 col-xs-6" style="height: 30%;padding-left: 8%;">
+                            <p style="font-size:21px;padding-top: 2%;"><b>2018. All Rights Reserved</b></p>
+                        </div>
+                        <div class="col-md-6 col-sm-6 col-xs-6" style="height: 30%;padding-right: 8%;">
+                            <p style="font-size:21px;padding-top: 2%;text-align: right"><b>RADEL CORP.</b></p>
+                        </div>
+                    </div>
+                </div>           <!-- Footer : Darshan's Code  -->
+            </div>  
+        </div>
+            </body>
+            </html>
