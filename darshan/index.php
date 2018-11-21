59b197d885c7db4c7d8397c46bfdcbfefcf82359
Darshan Ail <44797504+darsh1076@users.noreply.github.com>
2018-11-21 14:20:01 +0530

    Update index.php

--- darshan/index.php
+++ darshan/index.php
@@ -18,13 +18,12 @@
             }
             .social { color:#ffffff;
             }
-            a { color:#E0E0E0   ;
-            }
+
             /*            SHADOW_FOR_TILES*/
             .shadow {
                 box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
             }
-            .row {
+            .rowAlign {
                 margin-top: 20px;
                 margin-left: 20px;
                 margin-right: 20px;
@@ -42,12 +41,17 @@
                 padding: 7px 28px;
             }
 
-            .header {
-                padding-top: 40px;
+            .head {
+                padding-top: 30px;
             }
 
             /*            ANIMATION_FOR_OUR_LISTING*/
             .zoom {
+                padding-left: 0px;
+                height: 350px;
+                background-repeat: no-repeat;
+                background-size: cover;
+                border: 2px solid white;
                 background-color: green;
                 transition: transform .2s;
                 margin: 0 auto;
@@ -60,6 +64,7 @@
                 z-index: 2;
                 cursor: pointer;
             }
+
 
             /*            GRID_CENTERED*/
             .row-centered {
@@ -74,97 +79,114 @@
                 background-color: #ccc;
             }
 
-            /*            HEIGHT_DEFINED_FOR_GRID*/
-            .col-lg-6 {
-                height: 250px;
-            }
-            .col-lg-3 {
-                height: 250px;
-            }
-            .col-md-6 {
-                height: 250px;
-            }
-            .col-md-3 {
-                height: 250px;
-            }
-            /*            .col-sm-3 {
-                            height: 150px;
-                        }
-                        .col-sm-6 {
-                            height: 150px;
-                        }
-                        .col-xs-3 {
-                            height: 100px;
-                        }
-                        .col-xs-6 {
-                            height: 100px;
-                        }*/
-            .footer {
+
+            .footertext {
                 color: white
             }
 
             /*            PLACEHOLDER_COLOR_CHANGE*/
             ::-webkit-input-placeholder { 
                 color: #fff;
-                text-transform: uppercase;
+                font-size: 13px;
             }
 
             /*            SEARCH_BUTTON_STYLING*/
             .btn {
                 background-color: #4CAF50;
             }
-            button {
-                -webkit-transition-duration: 0.4s; /* Safari */
+            buttonEffect {
+                -webkit-transition-duration: 0.4s; 
                 transition-duration: 0.4s;
             }
 
-            button:hover {
+            buttonEffect:hover {
                 background-color: #2E7D32; /* Green */
                 color: white;
             }
+            u {
+                text-decoration: none;
+                border-bottom: 1px solid white;
+            }
+            .featured {
+                background-repeat: no-repeat;
+                background-size: cover;
+                border: 2px solid white;
+                height: 250px;
+            }
+            .featured:hover {
+                opacity: 0.5;
+                transition-duration: 0.4 s;
+                cursor: pointer;
+
+            }
+            .overlaytext {
+                position: absolute;
+                bottom: 0;
+                background-color: black;
+                background: rgba(0, 0, 0, 0.7);
+                width: 100%;
+                height: 15%;
+                font-size: 17px;
+                color: white;
+                margin-top: 4%;
+            }
+            .footertitle {
+                font-size:18px;
+                padding-top: 10%;
+            }
+            .footerSubHeadings {
+                font-size:13px;
+                padding-top: 1%
+            }
+            .linktag {
+                font-size:18px;
+                color: grey
+            }
         </style>
-        <div class="row">
+        <div class="row rowAlign">
             <div class="col-lg-12 shadow" style="background-color:black;height: 70px;">
                 <center><h2>PropKorner Header</h2></center>
             </div>
         </div>
-        <div class="row shadow">
+        <div class="row rowAlign shadow">
             <div class="col-md-12" style="background: rgba(0, 0, 0, 0.8);height: 175px;padding-left: 0px;padding-right: 0px;">
 
                 <div class="col-md-5" style="height: 100%;">
                     <center>
-                        <p class="header" style="color:black;">
-                            <a href="#" style="font-size:18px;"><b>For Sale </b></a>/
-                            <a href="#" style="font-size:18px;"><b>For Rent</b></a>
-                            <br>
-                            <input class="search" type="texbox" style="background: transparent; border: 1px solid #fff;font-size: 14px;" placeholder="Locality, Pincode, Name etc." size="42%">
-                            <button class="btn">Search</button>
-                            <br>
+                        <p class="head" style="color:white;">
+                        <div style="color:white;text-align: left;padding-left: 9%;padding-bottom: 2px;">
+                            <a href="#" class="linktag"><u>For Sale </u></a>/
+                            <a href="#" class="linktag"><u>For Rent</u></a>
+                        </div>
+                        <input class="search" type="texbox" style="background: transparent; border: 1px solid #fff;font-size: 14px;padding-bottom: 10px;margin-right: -4px;height: 40px;color: white;" placeholder="Locality, Pincode, Name etc." size="42%">
+                        <button class="btn buttonEffect" style=" border: 1px solid #fff;border-radius: 0px;height: 40px;">Search</button>
+                        <div style="color:white;text-align: left;padding-left: 14%;">
                             <span id="typed" style="white-space:pre;font-size: 20px;color: white" class="typed">
-                            </span>
+                            </span
+                        </div>
                         </p>
                     </center>
                 </div>
 
                 <div class="col-md-7" style="height: 100%;">
                     <center>
-                        dfsdf
+                        
                     </center>
                 </div>
             </div>
         </div>
 
-        <div class="row" style="margin-top: 20px">
-            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 shadow" style="background-image:url(images/1.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;"></div>
-            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 shadow" style="background-image:url(images/2.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;"></div>
-            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 shadow" style="background-image:url(images/3.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;"></div>
-            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 shadow" style="background-image:url(images/4.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;"></div>
+        <div class="row rowAlign" style="margin-top: 20px">
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 shadow featured" style="background-image:url(images/1.jpg);"></div>
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 shadow featured" style="background-image:url(images/2.jpg);"></div>
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 shadow featured" style="background-image:url(images/3.jpg);"></div>
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 shadow featured" style="background-image:url(images/4.jpg);"></div>
         </div>
 
-        <div class="row" style="margin-top: 5px">
-            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 shadow" style="background-image:url(images/5.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;"></div>
-            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 shadow" style="background-image:url(images/6.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;"></div>
-            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 shadow" style="background-image:url(images/2.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;"></div>
+        <div class="row rowAlign" style="margin-top: 8px">
+            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 shadow featured" style="background-image:url(images/5.jpg);"></div>
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 shadow featured" style="background-image:url(images/6.jpg);"></div>
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 shadow featured" style="background-image:url(images/2.jpg);"></div>
         </div>
 
         <div style="padding-top:20px;">
@@ -172,65 +194,76 @@
                 <h4 style="padding-top:50px; font-size: 27px">OUR LISTINGS</h4>
             </center>
         </div>
-        <div class="row row-centered" style="margin-top: 40px;text-align: center">
+        <div class="row rowAlign row-centered" style="margin-top: 40px;text-align: center">
 
-            <div class="col-md-2 col-sm-4 col-xs-6 zoom shadow col-centered" style="padding-left: 0px;height: 350px;background-image:url(images/apartments.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;">
-                <div style="position: absolute;bottom: 0;background-color: black;background: rgba(0, 0, 0, 0.7);width: 100%;height: 15%;"><p style="font-size: 17px;color: white;margin-top: 4%;">Apartments</p></div>
+            <div class="col-md-2 col-sm-4 col-xs-6 zoom shadow col-centered" style="background-image:url(images/apartments.jpg);">
+                <div class="overlaytext"><p style="font-size: 17px;color: white;margin-top: 4%;">Apartments</p></div>
             </div>
-            <div class="col-md-2 col-sm-4 col-xs-6 zoom shadow col-centered" style="padding-left: 0px;height: 350px;background-image:url(images/villa.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;position: relative">
-                <div style="position: absolute;bottom: 0;background-color: black;background: rgba(0, 0, 0, 0.7);width: 100%;height: 15%;"><p style="font-size: 17px;color: white;margin-top: 4%;">Villas</p></div>
+            <div class="col-md-2 col-sm-4 col-xs-6 zoom shadow col-centered" style="background-image:url(images/villa.jpg);">
+                <div class="overlaytext""><p style="margin-top: 4%;">Villas</p></div>
             </div>
-            <div class="col-md-2 col-sm-4 col-xs-6 zoom shadow col-centered" style="padding-left: 0px;height: 350px;background-image:url(images/plot.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;">
-                <div style="position: absolute;bottom: 0;background-color: black;background: rgba(0, 0, 0, 0.7);width: 100%;height: 15%;"><p style="font-size: 17px;color: white;margin-top: 4%;">Plots</p></div>
+            <div class="col-md-2 col-sm-4 col-xs-6 zoom shadow col-centered" style="background-image:url(images/plot.jpg);">
+                <div class="overlaytext""><p style="margin-top: 4%;">Plots</p></div>
             </div>
-            <div class="col-md-2 col-sm-4 col-xs-6 zoom shadow col-centered" style="padding-left: 0px;height: 350px;background-image:url(images/officespaces.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;">
-                <div style="position: absolute;bottom: 0;background-color: black;background: rgba(0, 0, 0, 0.7);width: 100%;height: 15%;"><p style="font-size: 17px;color: white;margin-top: 4%;">Office  Spaces</p></div>
+            <div class="col-md-2 col-sm-4 col-xs-6 zoom shadow col-centered" style="background-image:url(images/officespaces.jpg);">
+                <div class="overlaytext""><p style="margin-top: 4%;">Office  Spaces</p></div>
             </div>
-            <div class="col-md-2 col-sm-8 col-xs-6 zoom shadow col-centered" style="padding-left: 0px;height: 350px;background-image:url(images/more.jpg);background-repeat: no-repeat;background-size: cover;border: 2px solid white;">
-                <div style="position: absolute;height: 100%;background-color: black;background: rgba(0, 0, 0, 0.6);width: 100%;height: 100%;"><p style="font-size: 27px;color: white;margin-top: 50%">More<hr width="15%"></p></div>
+            <div class="col-md-2 col-sm-8 col-xs-6 zoom shadow col-centered" style="background-image:url(images/more.jpg);">
+                <div class="overlaytext" style="height: 100%;"><p style="font-size: 27px;color: white;margin-top: 50%">More<hr width="15%"></p></div>
             </div>
 
         </div>
-        <div class="footer">
-            <div class="row shadow">
+        <div class="footertext">
+            <div class="row rowAlign shadow">
                 <div class="col-md-12 col-sm-12" style="background-color:black;height: 350px;">
                     <div class="col-md-3 col-sm-3 col-xs-3" style="height: 70%;padding-left: 8%">
-                        <p style="font-size:18px;padding-top: 10%;"><b>EXPLORE</b></p>
+                        <p class="footertitle"><b>EXPLORE</b></p>
                         <hr style="margin-top:1%" width="15%;" align="left">
-                        <p style="font-size:13px;padding-top: 1%">HOME</p>
-                        <p style="font-size:13px;padding-top: 1%">ABOUT US</p>
-                        <p style="font-size:13px;padding-top: 1%">CAREER</p>
-                        <p style="font-size:13px;padding-top: 1%">BLOGS</p>
+                        <p class="footerSubHeadings">HOME</p>
+                        <p class="footerSubHeadings">ABOUT US</p>
+                        <p class="footerSubHeadings">CAREER</p>
+                        <p class="footerSubHeadings">BLOGS</p>
                     </div>
                     <div class="col-md-3 col-sm-3 col-xs-3" style="height: 70%;padding-left: 8%">
-                        <p style="font-size:18px;padding-top: 10%"><b>VISIT</b></p>
+                        <p class="footertitle"><b>VISIT</b></p>
                         <hr style="margin-top:1%" width="15%;" align="left">
-                        <p style="font-size:13px;padding-top: 1%"> No 2, Ground floor,<br> 29th Main Road,<br> Kuvempu Nagar,<br> BTM Layout 2nd Stage,<br> Bengaluru,<br> Karnataka 560076</p>
+                        <p class="footerSubHeadings"> No 2, Ground floor,<br> 29th Main Road,<br> Kuvempu Nagar,<br> BTM Layout 2nd Stage,<br> Bengaluru,<br> Karnataka 560076</p>
                     </div>
                     <div class="col-md-3 col-sm-3 col-xs-3" style="height: 70%;padding-left: 8%">
-                        <p style="font-size:18px;padding-top: 10%"><b>CONTACT US</b></p>
+                        <p class="footertitle"><b>CONTACT US</b></p>
                         <hr style="margin-top:1%" width="15%;" align="left">
-                        <p style="font-size:13px;padding-top: 1%">+91-8945687598</p>
-                        <p style="font-size:13px;padding-top: 1%">+91-8945687598</p>
+                        <p class="footerSubHeadings">+91-8945687598</p>
+                        <p class="footerSubHeadings">+91-8945687598</p>
                     </div>
                     <div class="col-md-3 col-sm-3 col-xs-3" style="height: 70%;padding-left: 8%">
-                        <p style="font-size:18px;padding-top: 10%"><b>FOLLOW</b></p>
+                        <p class="footertitle"><b>FOLLOW</b></p>
                         <hr style="margin-top:1%" width="15%;" align="left">
-                        <p style="font-size:13px;padding-top: 1%"><a href="#" class="social">INSTAGRAM </a></p>
-                        <p style="font-size:13px;padding-top: 1%"><a href="#" class="social">TWITTER </a></p>
-                        <p style="font-size:13px;padding-top: 1%"><a href="#" class="social">GOOGLE+ </a></p>
-                        <p style="font-size:13px;padding-top: 1%"><a href="#" class="social">FACEBOOK </a></p>
+                        <p class="footerSubHeadings"><a href="#" class="social">INSTAGRAM </a></p>
+                        <p class="footerSubHeadings"><a href="#" class="social">TWITTER </a></p>
+                        <p class="footerSubHeadings"><a href="#" class="social">GOOGLE+ </a></p>
+                        <p class="footerSubHeadings"><a href="#" class="social">FACEBOOK </a></p>
                     </div>
                     <hr style="margin-top:1%" width="95%;">
                     <div class="col-md-6 col-sm-6 col-xs-6" style="height: 30%;padding-left: 8%;">
-                        <p style="font-size:19px;padding-top: 2%;"><b>2018. All Rights Reserved</b></p>
+                        <p class="footertitle" style="padding-top:2%">2018. All Rights Reserved</p>
                     </div>
                     <div class="col-md-6 col-sm-6 col-xs-6" style="height: 30%;padding-right: 8%;">
-                        <p style="font-size:19px;padding-top: 2%;text-align: right"><b>RADEL CORP.</b></p>
+                        <p class="footertitle" style="text-align: right;padding-top: 2%">RADEL CORP.</p>
                     </div>
                 </div>
             </div>
-
+            <script>
+                $("#typed").typed({
+                    strings: ["Apartments.", "572306.", "JP Nagar 5th phase."],
+                    typeSpeed: 130,
+                    startDelay: 0,
+                    backSpeed: 100,
+                    backDelay: 100,
+                    loop: true,
+                    cursorChar: "|",
+                    contentType: 'html'
+                });
+            </script>
 
 
 
