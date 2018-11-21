bc882e1b85e4b24654b22db8878ff2722aef61a1
jintuabraham thomas <44797802+jintuabraham@users.noreply.github.com>
2018-11-21 13:46:02 +0530

    Create contact.php
    updated contact

---
+++ jintu/contact.php
@@ -0,0 +1,54 @@
+<html>
+    <head>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed" rel="stylesheet">
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">
+        </script>
+
+    </head>
+    <body>
+
+
+        <div class="row">
+            <div class="col-md-12" style="background-color: grey;background-image: url( 1.jpg);height: 900px">
+                <div class="col-md-2"style="background-color: white;height: 900px"></div>
+                <div class="col-md-10"style="background-color: red;height: 50px"></div>
+                <div class="col-md-12" style="background-color: grey;height: 850px">
+                    <div class="col-md-4"style="background-image: url( 1.jpg);height: 200px"> 
+                    </div>
+                    <div class="col-md-4"style=";background-image: url( 12.jpg);height: 200px;"> 
+                    </div>
+                    <div class="col-md-4"style=";background-image: url( 123.jpg);height:200px;"> 
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-12" style="background-color: grey;background-image: url( 12.jpg);height: 900px">
+
+                <div class="col-md-4"style="background-image: url( 12.jpg);height: 200px"> 
+                </div>
+                <div class="col-md-4"style=";background-image: url( 1.jpg);height: 200px;"> 
+                </div>
+                <div class="col-md-4"style=";background-image: url( 12.jpg);height:200px;"> 
+                </div>
+            </div>
+            <div class="col-md-12" style="background-color: grey;background-image: url( 123.jpg);height: 900px">
+
+                <div class="col-md-4"style="background-image: url( 12.jpg);height: 200px"> 
+                </div>
+                <div class="col-md-4"style=";background-image: url( 123.jpg);height: 200px;"> 
+                </div>
+                <div class="col-md-4"style=";background-image: url( 1.jpg);height:200px;"> 
+                </div>
+            </div>
+            
+        </div>
+
+    </body>
+
+</html>
