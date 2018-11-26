b7335053a325ea3842b51b1c8f95aba5f5a66a3f
jintuabraham thomas <44797802+jintuabraham@users.noreply.github.com>
2018-11-26 12:13:27 +0530

    update

---
+++ jintu/search.php
@@ -0,0 +1,105 @@
+<html>
+    <head>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+
+        <!-- Latest compiled JavaScript -->
+        <script type="text/javascript" src="https://gc.kis.v2.scr.kaspersky-labs.com/EC6EB173-5F12-2547-809A-0D85FA66C27C/main.js" charset="UTF-8"></script><script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    </head>
+    <body>
+
+        <style type="text/css">
+
+            body{
+                font-family: 'Roboto', sans-serif;
+            }
+            
+            .picture_box .col-md-4{
+                margin-bottom: 30px; 
+            }
+
+            .picture_box{
+                border-left: 1px solid #ccc; 
+                padding-top: 30px; 
+            }
+
+            .search_box{
+                border-bottom: 1px solid #ccc; 
+                border-left: 1px solid #ccc; 
+            }
+
+        </style>
+
+
+
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-2 category_box">
+                    <h3>Catagories</h3>
+                </div>
+               
+                <div class="col-md-10">
+                    <div class="col-md-12 search_box">
+                        <h3>Search</h3>
+                    </div>     
+                    <div class="col-md-12 picture_box">
+                        <div class="col-md-4"> 
+                            <img src="1.jpg" width="100%">
+                        </div>
+                        
+                        <div class="col-md-4"> 
+                            <img src="1.jpg" width="100%">
+                        </div>
+
+                        <div class="col-md-4"> 
+                            <img src="1.jpg" width="100%">
+                        </div>
+
+                        <div class="col-md-4"> 
+                            <img src="1.jpg" width="100%">
+                        </div>
+
+                        <div class="col-md-4"> 
+                            <img src="1.jpg" width="100%">
+                        </div>
+
+                        <div class="col-md-4"> 
+                            <img src="1.jpg" width="100%">
+                        </div>
+
+                        <div class="col-md-4"> 
+                            <img src="1.jpg" width="100%">
+                        </div>
+
+                        <div class="col-md-4"> 
+                            <img src="1.jpg" width="100%">
+                        </div>
+
+                        <div class="col-md-4"> 
+                            <img src="1.jpg" width="100%">
+                        </div>
+
+                        <div class="col-md-4"> 
+                            <img src="1.jpg" width="100%">
+                        </div>
+
+                        <div class="col-md-4"> 
+                            <img src="1.jpg" width="100%">
+                        </div>
+
+                        <div class="col-md-4"> 
+                            <img src="1.jpg" width="100%">
+                        </div>  
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        
+
+    </body>
+
+</html>
