1a359bbdcacb141a315c2b36778363fb94b7e117
Akhil P Nair <pakhilnair@gmail.com>
2019-01-02 16:01:56 +0530

    Whatsapp Link

---
+++ akhil/whatsapp_full.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Whatsapp Element</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <div class="col-xs-12" style="background-color: #e4e4e4; height: 35px; width:150px; border-radius: 25px; padding-left: 8px;">
+            <a href="https://api.whatsapp.com/send?phone=919845044599"><div class="col-xs-3"  style="height:30px; background-image: url(img/1.png); background-repeat: no-repeat; background-size: 90%; margin-top:2px"></div>
+                <div class="col-xs-9" style="font-weight: bold; font-size : 10px; margin-top: 10px; width: max-content; ">Whatsapp Us</div>
+            </a>
+        </div>
+    </body>
---
+++ akhil/whatsapp_mob.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Whatsapp Element</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <div style="background-color: #e4e4e4; height: 35px; width:40px; border-radius: 25px; padding-left: 5px; padding-top: 1px">
+            <a href="https://api.whatsapp.com/send?phone=919845044599">
+                <div style="height:30px; background-image: url(img/1.png); background-repeat: no-repeat; background-size: 85%; margin-top: 1px "></div>
+            </a>
+        </div>
+    </body>
